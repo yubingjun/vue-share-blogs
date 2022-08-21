f88e3da0d6802cfb767842bcd23abff3ad0f2559
xiyou <494949543@qq.com>
2022-08-21 12:29:47 +0800

    完成 Login no-Login 的样式

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,16 @@
 <template>
   <footer>
-    <p>&copy; 幼儿园扛把子 2022 WeChat:LF677880</p>
+    <p>&copy; 幼儿园扛把子 &nbsp&nbsp 2022 &nbsp&nbsp WeChat：LF677880</p>
   </footer>
 </template>
+
+<style lang="less">
+  footer {
+    align-self: end;
+    background-color: #d7d7d7;
+    color: #666;
+    font-size: 13px;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
